b48451634b8cbcbe96d5ab4b30bdd6afde8ce970
kiennt <kiennt@nec.vn>
2025-04-16 13:58:23 +0700

    cdk run

--- lib/github-aws-cicd-stack.ts
+++ lib/github-aws-cicd-stack.ts
@@ -1,16 +1,26 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, SecretValue } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
-export class GithubAwsCicdStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class GithubAwsCicdStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const githubToken = SecretValue.secretsManager('arn:aws:secretsmanager:ap-southeast-1:767397656228:secret:githubkiennt308token-FQ6VnY');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GithubAwsCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'MyPipeline', {
+      pipelineName: 'MyCDKPipeline',
+      synth: new ShellStep('SynthStep', {
+        input: CodePipelineSource.gitHub('dangtrinhhh/learn-cdk', 'main', {
+          authentication: githubToken
+        }),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cdk.out'
+      })
+    });
   }
 }
